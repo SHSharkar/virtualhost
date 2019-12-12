e604eec67a0aa6e05cd0a7214876e8cddbc99687
Sazzad Hossain Sharkar <sh@sharkar.net>
2019-12-12 18:24:03 +0000

    Update source code for personal configuration

--- virtualhost.sh
+++ virtualhost.sh
@@ -95,24 +95,24 @@ if [ "$action" == 'create' ]
 		fi
 
 		### Add domain in /etc/hosts
-		if ! echo "127.0.0.1	$domain" >> /etc/hosts
-		then
-			echo $"ERROR: Not able to write in /etc/hosts"
-			exit;
-		else
-			echo -e $"Host added to /etc/hosts file \n"
-		fi
+		##if ! echo "127.0.0.1	$domain" >> /etc/hosts
+		##then
+			##echo $"ERROR: Not able to write in /etc/hosts"
+			##exit;
+		##else
+			##echo -e $"Host added to /etc/hosts file \n"
+		##fi
 
 		### Add domain in /mnt/c/Windows/System32/drivers/etc/hosts (Windows Subsytem for Linux)
-		if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
-		then
-			if ! echo -e "\r127.0.0.1       $domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
-			then
-				echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts (Hint: Try running Bash as administrator)"
-			else
-				echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
-			fi
-		fi
+		##if [ -e /mnt/c/Windows/System32/drivers/etc/hosts ]
+		##then
+			##if ! echo -e "\r127.0.0.1       $domain" >> /mnt/c/Windows/System32/drivers/etc/hosts
+			##then
+				##echo $"ERROR: Not able to write in /mnt/c/Windows/System32/drivers/etc/hosts (Hint: Try running Bash as administrator)"
+			##else
+				##echo -e $"Host added to /mnt/c/Windows/System32/drivers/etc/hosts file \n"
+			##fi
+		##fi
 
 		if [ "$owner" == "" ]; then
 			iam=$(whoami)
